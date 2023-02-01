153d701d48e7b9f72a06968737974e5fa7942bc1
Ariel <ariel.calderon@epn.edu.ec>
2023-02-01 05:15:04 -0500

    cambios necesarios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.example.dailymoments',
-  appName: 'daily-moments',
+  appId: 'epn.edu.tesis',
+  appName: 'tesis',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
