80cd34501dfeabb48385de018617c518defb18a2
Ariel <ariel.calderon@epn.edu.ec>
2023-02-15 15:15:39 -0500

    ajuste menor

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ import "./theme/variables.css"
 const App: React.FC = () => {
 
   //Se crean variables con el hook useState para declarar el estado inicial de inicio de sesion del usuario
-  const [loggedIn, setLoggedIn] = useState(true);
-  const [role, setRole] = useState("teacher");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [role, setRole] = useState("");
   
   
   
